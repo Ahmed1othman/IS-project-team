db66af1514ad46f62cdbb305802ac7c3354a9e39
Ahmed <Ahmed@172.31.200.157>
2016-03-22 10:39:31 +0200

    add no thing

---
+++ src/ex/problem.java
@@ -0,0 +1,18 @@
+package ex;
+
+import java.util.Scanner;
+
+public class problem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long minute,hour,second;
+		long time = System.currentTimeMillis(); 
+		long totalsecond= time/1000;
+		second=totalsecond%60;
+		int x= -2147483648 - 2;
+		
+		System.out.println(x);
+	}
+
+}
